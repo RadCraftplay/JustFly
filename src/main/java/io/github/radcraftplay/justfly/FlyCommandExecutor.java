0c82ba63cedece63605a2566034ac62a59a17919
RadCraftplay <radcraftplay2@gmail.com>
2020-05-25 21:24:45 +0200

    Refactored FlyCommandExecutor

--- src/main/java/io/github/radcraftplay/justfly/FlyCommandExecutor.java
+++ src/main/java/io/github/radcraftplay/justfly/FlyCommandExecutor.java
@@ -18,37 +18,49 @@ public class FlyCommandExecutor implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player target = null;
-
         if (!command.getName().equalsIgnoreCase("fly"))
             return false;
-
-        if (args.length == 1) {
-            if (!checkPermission(sender, "justfly.flyothers"))
-                return true;
-            target = _server.getPlayer(args[0]);
-        } else if (args.length == 0) {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage("This command can be run only by players!");
-                return true;
-            }
-            if (!checkPermission(sender, "justfly.fly"))
-                return true;
-
-            target = (Player)sender;
-        } else {
-            return false;
-        }
-
-        if (target == null) {
-            sender.sendMessage(String.format("Unable to find player with nickname '%s'", args[0]));
-            return false;
-        }
 
         if (!_server.getAllowFlight()) {
             sender.sendMessage(ChatColor.RED + "Flying is disabled on this server!");
             return true;
         }
+
+        if (args.length == 0) {
+            return flySelf(sender);
+        } else if (args.length == 1) {
+            Player target = _server.getPlayer(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(String.format("Unable to find player with nickname '%s'", args[0]));
+                return false;
+            }
+
+            return flyOther(sender, target);
+        } else {
+            return false;
+        }
+    }
+
+    Boolean checkPermission(CommandSender sender, String permission) {
+        if (!sender.hasPermission(permission)) {
+            sender.sendMessage(NO_PERMISSIONS_MESSAGE);
+            return false;
+        }
+
+        return true;
+    }
+
+    boolean flySelf(CommandSender sender) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can be run only by players!");
+            return true;
+        }
+
+        if (!checkPermission(sender, "justfly.fly"))
+            return true;
+
+        Player target = (Player)sender;
 
         if (target.getAllowFlight()) {
             target.setAllowFlight(false);
@@ -62,10 +74,42 @@ public class FlyCommandExecutor implements CommandExecutor {
         return true;
     }
 
-    Boolean checkPermission(CommandSender sender, String permission) {
-        if (!sender.hasPermission(permission)) {
-            sender.sendMessage(NO_PERMISSIONS_MESSAGE);
-            return false;
+    boolean flyOther(CommandSender sender, Player target) {
+        if (sender instanceof Player) {
+            Player source = (Player)sender;
+
+            if (source.getName().equals(target.getName())) {
+                return flySelf(sender);
+            }
+        }
+
+        if (!checkPermission(sender, "justfly.flyothers"))
+            return true;
+
+
+        if (target.getAllowFlight()) {
+            target.setAllowFlight(false);
+            sender.sendMessage("Flying is now"
+                    + ChatColor.RED
+                    + " disabled"
+                    + ChatColor.GRAY +
+                    " for "
+                    + ChatColor.BLUE
+                    + target.getName());
+
+            target.sendMessage("Flying is now" + ChatColor.RED + " disabled!");
+        }
+        else {
+            target.setAllowFlight(true);
+            sender.sendMessage("Flying is now"
+                    + ChatColor.GREEN
+                    + " enabled"
+                    + ChatColor.GRAY +
+                    " for "
+                    + ChatColor.BLUE
+                    + target.getName());
+
+            target.sendMessage("Flying is now" + ChatColor.GREEN + " enabled!");
         }
 
         return true;
